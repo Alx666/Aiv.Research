a6e206d0fcfa1119341ff19ddc6a828bf0a18ea2
redsanso <redsanso@gmail.com>
2017-05-24 20:42:00 +0200

    TrainingService

---
+++ Aiv.Research.TrainingServer/TrainingService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aiv.Research.Shared;
+using System.ServiceModel;
+
+namespace Aiv.Research.TrainingServer
+{
+    [ServiceBehavior]
+    class TrainingService : ITrainingService
+    {
+        public TrainingService()
+        {
+
+        }
+
+        [ConsoleUIMethod]
+        public void DeleteTraining(int iConfigId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ConsoleUIMethod]
+        public byte[] DownloadTrainingData(int iConfigId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ConsoleUIMethod]
+        public IEnumerable<NetworkCreationConfig> EnumerateTrainingsCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        [ConsoleUIMethod]
+        public IEnumerable<NetworkCreationConfig> EnumerateTrainingsInProgress()
+        {
+            throw new NotImplementedException();
+        }
+
+        [ConsoleUIMethod]
+        public void StartTraining(NetworkCreationConfig hNetwork, int iIterations, string sConfig)
+        {
+            throw new NotImplementedException();
+        }
+
+        [ConsoleUIMethod]
+        public void TerminateTraining(int iConfigId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
