3fad23dba5725f11b20489f83aec8ef140ffa6fd
alxeyesoul <Alx666@DESKTOP-Q2ITAPK>
2017-05-24 20:26:51 +0200

    Service Interface

--- Aiv.Research.Shared/NetworkCreationConfig.cs
+++ Aiv.Research.Shared/NetworkCreationConfig.cs
@@ -4,10 +4,13 @@ using System.Text;
 using System.Threading.Tasks;
 using Encog.Engine.Network.Activation;
 using System;
+using System.ServiceModel;
+using System.Runtime.Serialization;
 
 namespace Aiv.Research.Shared
 {
     [Serializable]
+    [DataContract]    
     public class NetworkCreationConfig
     {
         public NetworkCreationConfig()
@@ -15,17 +18,34 @@ namespace Aiv.Research.Shared
             Samples = new List<Sample>();
         }
 
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
         public int InputSize { get; set; }
+        [DataMember]
         public int OutputSize { get; set; }
+        [DataMember]
         public int HL0Size { get; set; }
+        [DataMember]
         public int HL1Size { get; set; }
+        [DataMember]
         public int HL2Size { get; set; }
         public IActivationFunction Activation { get; set; }
+        [DataMember]
         public bool Visualize { get; set; }
+        [DataMember]
         public int Width { get; set; }
+        [DataMember]
         public int Height { get; set; }
+        [DataMember]
         public int NeuronSize { get; set; }
 
+        [DataMember]
         public List<Sample> Samples { get; set; }
+
+
+        public override string ToString() => $"[{Id}] {Name}";
     }
 }
---
+++ Aiv.Research.TrainingServer/ITrainingService.cs
@@ -0,0 +1,42 @@
+﻿using Aiv.Research.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aiv.Research.TrainingServer
+{
+    [ServiceContract]
+    public interface ITrainingService
+    {
+        
+        //Ad ogni chiamata viene creato un Thrad (meglio se usate i Task long running, max 4 attivi)
+        //Se sono più di 4 vengono messi in una BlockingCollection<T>
+        //Lavora i dati (mette eventuali oggetti in una lista apposita List<T> lokkata
+        //Terminato il task deve classficicare i dati su filesystem (classe statica Classifier che si occupa di salvare i dati su disco)
+        [OperationContract]
+        void StartTraining(NetworkCreationConfig hNetwork, int iIterations, string sConfig);
+
+        //ritorna lo stato attuale della lista contenente i task in corso
+        [OperationContract]
+        IEnumerable<NetworkCreationConfig> EnumerateTrainingsInProgress();
+
+        //Chiede al claffier di fornire tutti i dati che ha scritto (li legge sempre da disco)
+        [OperationContract]
+        IEnumerable<NetworkCreationConfig> EnumerateTrainingsCompleted();
+
+        //Ritorna solamente i dati della rete richiesta
+        [OperationContract]
+        byte[] DownloadTrainingData(int iConfigId);
+
+        //fa capo ad un cancellation token che viene controllato ad ogni iterazione del training
+        [OperationContract]
+        void TerminateTraining(int iConfigId);
+
+        //Cancella da disco il train con id specificato
+        [OperationContract]
+        void DeleteTraining(int iConfigId);
+    }
+}
