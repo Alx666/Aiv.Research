dcf6764293595e2b9e24f3101ee475f5bd380ddc
Alessio Greco <alxeyesoul@live.com>
2017-05-26 13:58:07 +0200

    some help with threading

--- Aiv.Research.TrainingServer/TrainingService.cs
+++ Aiv.Research.TrainingServer/TrainingService.cs
@@ -6,18 +6,26 @@ using System.Threading.Tasks;
 using Aiv.Research.Shared;
 using System.ServiceModel;
 using System.Collections.Concurrent;
+using System.Threading;
 
 namespace Aiv.Research.TrainingServer
 {
-    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, 
-                     InstanceContextMode = InstanceContextMode.Single, 
-                     AddressFilterMode = AddressFilterMode.Any)]
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple, InstanceContextMode = InstanceContextMode.Single, AddressFilterMode = AddressFilterMode.Any)]
     class TrainingService : ITrainingService
     {
-        private Task m_hTask;
-        public TrainingService()
+        private Task                                                m_hDispatcherTask;
+        private BlockingCollection<NetworkCreationConfig>           m_hNetworksToTrain;
+        private ConcurrentDictionary<NetworkCreationConfig, int>    m_hTrainingInProgress;
+        private CancellationTokenSource                             m_hDispatcherTakeToken;
+        private int                                                 m_iMaxParallelTrainings;
+
+        public TrainingService(int iMaxParallelTrainings)
         {
-            m_hTask = Task.Factory.StartNew(metodo, cancellationToken, TaskCreationOptions.LongRunning);
+            m_iMaxParallelTrainings = iMaxParallelTrainings;
+            m_hNetworksToTrain      = new BlockingCollection<NetworkCreationConfig>(iMaxParallelTrainings);
+            m_hTrainingInProgress   = new ConcurrentDictionary<NetworkCreationConfig, int>();
+            m_hDispatcherTakeToken  = new CancellationTokenSource();
+            m_hDispatcherTask       = Task.Factory.StartNew(DispatcherRoutine, null, TaskCreationOptions.LongRunning);
         }
 
         [ConsoleUIMethod]
@@ -44,11 +52,17 @@ namespace Aiv.Research.TrainingServer
             throw new NotImplementedException();
         }
 
-        BlockingCollection<NetworkCreationConfig> m_hCollection;
+      
         [ConsoleUIMethod]
         public void StartTraining(NetworkCreationConfig hNetwork, int iIterations, string sConfig)
         {
-            m_hCollection.Add(hNetwork);
+            if (m_hNetworksToTrain.Count >= m_iMaxParallelTrainings)
+            {
+                //non possiamo mettere altre cose a lavoro (eccezione, fault channel, o cosa???)
+
+            }
+
+            m_hNetworksToTrain.Add(hNetwork); //salvare anche iterations ed eventuali altri dati
         }
 
         [ConsoleUIMethod]
@@ -57,13 +71,29 @@ namespace Aiv.Research.TrainingServer
             throw new NotImplementedException();
         }
 
-        private void InputDataManager()
-        {
-            while(cancellation token)
-            {
-                m_hCollection.Take(); //sta sempre fermo qui
 
+        private void DispatcherRoutine(object hParam)
+        {
+            while(true)
+            {
+                NetworkCreationConfig hJob = m_hNetworksToTrain.Take(m_hDispatcherTakeToken.Token); //dispatcher thread wait here for a job to do
+
+                Task hTraining = Task.Factory.StartNew(NetworkTrainingRoutine, hJob, TaskCreationOptions.LongRunning);     
             }
+        }
+
+
+        private void NetworkTrainingRoutine(object hParam)
+        {
+            NetworkCreationConfig hWorkItem = hParam as NetworkCreationConfig;
+
+            m_hTrainingInProgress.TryAdd(hWorkItem, 0);
+
+            //Looooong Training here
+
+            m_hTrainingInProgress.TryRemove(hWorkItem, out int iValue);
+
+            //Send to Classifier
         }
     }
 }
