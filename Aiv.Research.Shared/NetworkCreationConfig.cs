83cde38e47c42c1f5053c92d4bf726830f6a4e69
Mattia10 <markeus@libero.it>
2017-05-31 19:50:10 +0200

    cicciofolder

--- Aiv.Research.Shared/NetworkCreationConfig.cs
+++ Aiv.Research.Shared/NetworkCreationConfig.cs
@@ -7,7 +7,6 @@ using System;
 using System.Reflection;
 using System.ServiceModel;
 using System.Runtime.Serialization;
-using Encog.Engine.Network.Activation;
 using System.Xml.Serialization;
 
 namespace Aiv.Research.Shared
--- Aiv.Research.TrainingServer/Program.cs
+++ Aiv.Research.TrainingServer/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO.Compression;
+using Aiv.Research.Shared;
 
 namespace Aiv.Research.TrainingServer
 {
