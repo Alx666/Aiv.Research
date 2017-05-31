cf70f5f70e69384baf6ededf03bc0d759d131df4
Mattia10 <markeus@libero.it>
2017-05-31 19:17:37 +0200

    merge part 2

--- Aiv.Research.TrainingServer/Program.cs
+++ Aiv.Research.TrainingServer/Program.cs
@@ -11,13 +11,14 @@ namespace Aiv.Research.TrainingServer
     {
         static void Main(string[] args)
         {
-            byte[] b = new byte[byte.MaxValue];
-            Shared.NetworkCreationConfig config = new Shared.NetworkCreationConfig();
-            Classifier.SetDataPath("C:/Users/marke/Desktop/Aiv.Research/Aiv.Research.TrainingServer/bin/Debug/cicciofolder/");
-            Classifier.Store(config, b);
+            //byte[] b = new byte[byte.MaxValue];
+            //Shared.NetworkCreationConfig config = new Shared.NetworkCreationConfig();
+            //Classifier.SetDataPath("C:/Users/marke/Desktop/Aiv.Research/Aiv.Research.TrainingServer/bin/Debug/cicciofolder/");
+            //Classifier.Store(config, b);
             //Classifier.Zip("C:/Users/marke/Desktop/Aiv.Research/Aiv.Research.TrainingServer/bin/Debug/zippofolder/");
-           // Classifier.Delete("zippofolder");
+            // Classifier.Delete("zippofolder");
+            //TrainingService service = new TrainingService(4);
         }
-        
+
     }
 }
