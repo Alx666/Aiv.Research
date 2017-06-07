e001822a8ae22c37d3fd4e7e37097de6108b2e7f
start <start@DESKTOP-JB5KGPT>
2017-06-07 18:45:33 +0200

    asd

---
+++ Aiv.Research.Visualizer2D/NeuronRenderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aiv.Fast2D;
+
+namespace Aiv.Research.Visualizer2D
+{
+    public class NeuronRenderer : Mesh
+    {
+        public NeuronRenderer() : base(null, 3)
+        {
+           
+        }
+
+
+        protected override void ApplyMatrix()
+        {
+            base.ApplyMatrix();
+        }
+        public void Draw()
+        {
+            this.Draw((m) =>
+           {
+
+           });
+        }
+    }
+}
