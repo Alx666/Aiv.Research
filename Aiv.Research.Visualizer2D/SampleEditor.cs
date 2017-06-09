e04f602e43f064957d5b6206991ef7158abe4663
start <start@DESKTOP-JB5KGPT>
2017-06-09 19:20:01 +0200

    Center()

--- Aiv.Research.Visualizer2D/SampleEditor.cs
+++ Aiv.Research.Visualizer2D/SampleEditor.cs
@@ -277,5 +277,92 @@ namespace Aiv.Research.Visualizer2D
         }
 
         #endregion
+
+
+        private void Center()
+        {
+            int iMinX = -1;
+            int iMaxX = -1;
+            int iMinY = -1;
+            int iMaxY = -1;
+
+            int centerX;
+            int centerY;
+
+            int distanceCenterX;
+            int distanceCenterY;
+
+            // BINDING BOX
+            int j = 0;
+            for (int i = 0; i < m_hInputData.GetLength(0); i++)
+            {
+                for (j = 0; j < m_hInputData.GetLength(1); j++)
+                {
+                    if (m_hInputData[i, j].InUse == true)
+                    {
+                        #region MinMaxXY
+                        if (iMinX != -1)
+                        {
+                            if (iMinX > i)
+                                iMinX = i;
+                        }
+                        else
+                            iMinX = i;
+
+                        if (iMaxX != -1)
+                        {
+                            if (iMaxX < i)
+                                iMaxX = i;
+                        }
+                        else
+                            iMaxX = i;
+
+                        if (iMinY != -1)
+                        {
+                            if (iMinY > j)
+                                iMinY = j;
+                        }
+                        else
+                            iMinY = j;
+
+                        if (iMaxY != -1)
+                        {
+                            if (iMaxY < j)
+                                iMaxY = j;
+                        }
+                        else
+                            iMaxY = j;
+                        #endregion
+                    }
+                }
+            }
+
+            // finding the center X and the center Y and distance from the center of the array
+            centerX = (iMinX + iMaxX) / 2;
+            centerY = (iMinY + iMaxY) / 2;
+            distanceCenterX = m_hInputData.GetLength(0) / 2 - centerX;
+            distanceCenterY = m_hInputData.GetLength(1) / 2 - centerY;
+
+            bool[,] boolArray = new bool[m_hInputData.GetLength(0), m_hInputData.GetLength(1)];
+
+            for (int i = 0; i < m_hInputData.GetLength(0); i++)
+            {
+                for (j = 0; j < m_hInputData.GetLength(1); j++)
+                {
+                    if (m_hInputData[i, j].InUse == true)
+                        boolArray[i + distanceCenterX, j + distanceCenterY] = true;
+                }
+            }
+            for (int i = 0; i < m_hInputData.GetLength(0); i++)
+            {
+                for (j = 0; j < m_hInputData.GetLength(1); j++)
+                {
+                    m_hInputData[i, j].InUse = false;
+                    if (boolArray[i, j] == true)
+                        m_hInputData[i, j].InUse = true;
+                }
+            }
+
+        }
     }
 }
