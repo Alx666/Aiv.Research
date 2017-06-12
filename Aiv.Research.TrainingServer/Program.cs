76c8f00a3a4c984e9473421f486da731b9c46e5b
Riccardo <ri.lissimo.rr@gmail.com>
2017-06-12 19:26:56 +0200

    Ri merge

--- Aiv.Research.TrainingServer/Program.cs
+++ Aiv.Research.TrainingServer/Program.cs
@@ -10,28 +10,7 @@ namespace Aiv.Research.TrainingServer
 {
     class Program
     {
-<<<<<<< HEAD
-        static void Main(string[] args) //=> new ConsoleUI(new TrainingService(4, 28999), "TrainingServer").RunAndWait();
-        {
-            byte[] b = new byte[byte.MaxValue];
-            Shared.NetworkCreationConfig config = new Shared.NetworkCreationConfig();
-            Random hRand = new Random();
 
-            for (int i = 101; i < 300; i++)
-            {
-                b = Enumerable.Range(5, 500).Select(x => (byte)hRand.Next()).ToArray();
-                config.Name = i.ToString();
-                config.Id = i;
-                Classifier.SetDataPath(".\\NeuralData\\Gianni"); 
-                Classifier.Store(config, b);
-                Classifier.Get(i);
-            }
-
-            //TrainingService service = new TrainingService(4);
-            //ConsoleUI console = new ConsoleUI(service, "TrainingServer");
-            //console.RunAndWait();
-        }
-=======
         static void Main(string[] args) => new ConsoleUI(new TrainingService(4), "Training Service").RunAndWait();
         //{
         //    byte[] b = new byte[byte.MaxValue];
@@ -54,7 +33,6 @@ namespace Aiv.Research.TrainingServer
         //    //ConsoleUI console = new ConsoleUI(service, "TrainingServer");
         //    //console.RunAndWait();
         //}
->>>>>>> aee36b9b3481758110af9876c988b46bd980211c
     }
         
 }
