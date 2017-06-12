559599c0be2805745baba971aefa24a1bde5a1b7
Riccardo <ri.lissimo.rr@gmail.com>
2017-06-12 18:32:52 +0200

    TrainingService Update

--- Aiv.Research.TrainingServer/Program.cs
+++ Aiv.Research.TrainingServer/Program.cs
@@ -10,28 +10,28 @@ namespace Aiv.Research.TrainingServer
 {
     class Program
     {
-        static void Main(string[] args) //=> new ConsoleUI(new TrainingService(4, 28999), "TrainingServer").RunAndWait();
-        {
-            byte[] b = new byte[byte.MaxValue];
-            Shared.NetworkCreationConfig config = new Shared.NetworkCreationConfig();
+        static void Main(string[] args) => new ConsoleUI(new TrainingService(4), "Training Service").RunAndWait();
+        //{
+        //    byte[] b = new byte[byte.MaxValue];
+        //    Shared.NetworkCreationConfig config = new Shared.NetworkCreationConfig();
 
-            Random hRand = new Random();
-            for (int i = 0; i < 5; i++)
-            {
-                b = Enumerable.Range(0, 500).Select(x => (byte)hRand.Next()).ToArray();
-                config.Name = i.ToString();
-                Classifier.SetDataPath(".\\NeuralData\\Gianni"); 
-                //Classifier.Store(config, b);
-                //Classifier.Get(i);
-                //Classifier.Get(i);
-            }
+        //    Random hRand = new Random();
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        b = Enumerable.Range(0, 500).Select(x => (byte)hRand.Next()).ToArray();
+        //        config.Name = i.ToString();
+        //        Classifier.SetDataPath(".\\NeuralData\\Gianni"); 
+        //        //Classifier.Store(config, b);
+        //        //Classifier.Get(i);
+        //        //Classifier.Get(i);
+        //    }
 
-            //Classifier.Get("0_");
+        //    //Classifier.Get("0_");
 
-            //TrainingService service = new TrainingService(4);
-            //ConsoleUI console = new ConsoleUI(service, "TrainingServer");
-            //console.RunAndWait();
-        }
+        //    //TrainingService service = new TrainingService(4);
+        //    //ConsoleUI console = new ConsoleUI(service, "TrainingServer");
+        //    //console.RunAndWait();
+        //}
     }
         
 }
